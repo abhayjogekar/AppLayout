207e04b0f3c1f7e29ea0c41438386c39f6734bd6
Abhay Jogekar <ajogekar@riverbed.com>
2017-04-09 05:00:37 +0530

    uploading to github

---
+++ app/src/main/java/com/example/abhay/applayout/SetContact.java
@@ -0,0 +1,13 @@
+package com.example.abhay.applayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_contact);
+    }
+}
